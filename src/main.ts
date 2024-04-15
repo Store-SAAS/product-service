8907aa40606e9758bf6a42d2f89b2e89e78032af
Carmelo Buelvas <cbuelvasc@gmail.com>
2024-04-15 09:51:26 -0500

    Refactor code formatting in main.ts and products.controller.ts

--- src/main.ts
+++ src/main.ts
@@ -7,29 +7,26 @@ import { AppModule } from './app.module';
 import { envs } from './config';
 
 async function bootstrap() {
-  const logger = new Logger('Main');
+	const logger = new Logger('Main');
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        port: envs.port,
-      },
-    },
-  );
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+		transport: Transport.TCP,
+		options: {
+			port: envs.port,
+		},
+	});
 
-  //app.setGlobalPrefix('api');
-  //app.enableCors();
-  //app.use(compression());
+	//app.setGlobalPrefix('api');
+	//app.enableCors();
+	//app.use(compression());
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-  await app.listen();
-  logger.log(`Products Microservice running on port ${envs.port}`);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
+	await app.listen();
+	logger.log(`Products Microservice running on port ${envs.port}`);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,13 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  ParseUUIDPipe,
-  Query,
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	ParseUUIDPipe,
+	Query,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -17,40 +17,40 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+	constructor(private readonly productsService: ProductsService) {}
 
-  //@Post()
-  @MessagePattern({ cmd: 'create-product' })
-  //create(@Body() createProductDto: CreateProductDto) {
-  create(@Payload() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
-  }
+	//@Post()
+	@MessagePattern({ cmd: 'create-product' })
+	//create(@Body() createProductDto: CreateProductDto) {
+	create(@Payload() createProductDto: CreateProductDto) {
+		return this.productsService.create(createProductDto);
+	}
 
-  //@Get()
-  @MessagePattern({ cmd: 'find-products' })
-  //findAll(@Query() paginationDto: PaginationDto) {
-  findAll(@Payload() paginationDto: PaginationDto) {
-    return this.productsService.findAll(paginationDto);
-  }
+	//@Get()
+	@MessagePattern({ cmd: 'find-products' })
+	//findAll(@Query() paginationDto: PaginationDto) {
+	findAll(@Payload() paginationDto: PaginationDto) {
+		return this.productsService.findAll(paginationDto);
+	}
 
-  //@Get(':id')
-  @MessagePattern({ cmd: 'find-product' })
-  //findOne(@Param('id', ParseUUIDPipe) id: string) {
-  findOne(@Payload('id', ParseUUIDPipe) id: string) {
-    return this.productsService.findOne(id);
-  }
+	//@Get(':id')
+	@MessagePattern({ cmd: 'find-product' })
+	//findOne(@Param('id', ParseUUIDPipe) id: string) {
+	findOne(@Payload('id', ParseUUIDPipe) id: string) {
+		return this.productsService.findOne(id);
+	}
 
-  //@Patch(':id')
-  @MessagePattern({ cmd: 'update-product' })
-  //update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
-  update(@Payload() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(updateProductDto.id, updateProductDto);
-  }
+	//@Patch(':id')
+	@MessagePattern({ cmd: 'update-product' })
+	//update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
+	update(@Payload() updateProductDto: UpdateProductDto) {
+		return this.productsService.update(updateProductDto.id, updateProductDto);
+	}
 
-  //@Delete(':id')
-  @MessagePattern({ cmd: 'remove-product' })
-  remove(@Payload('id', ParseUUIDPipe) id: string) {
-  //remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.productsService.remove(id);
-  }
+	//@Delete(':id')
+	@MessagePattern({ cmd: 'remove-product' })
+	remove(@Payload('id', ParseUUIDPipe) id: string) {
+		//remove(@Param('id', ParseUUIDPipe) id: string) {
+		return this.productsService.remove(id);
+	}
 }
